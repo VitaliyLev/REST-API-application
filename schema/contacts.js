435b05edcbb2d6f6b0794637bdcf74bca5f6c826
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-02-15 15:51:21 +0200

    add hw 03

--- schema/contacts.js
+++ schema/contacts.js
@@ -3,11 +3,12 @@ const Joi = require("joi");
 const contactsSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email({ minDomainSegments: 2 }).required(),
-  phone: Joi.string()
-    .pattern(/^[0-9()]+$/, "numbers")
-    .max(10)
-    .required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool(),
 });
+const updateStatusSchema = Joi.object({ favorite: Joi.boolean().optional() });
+
 module.exports = {
   contactsSchema,
+  updateStatusSchema,
 };
