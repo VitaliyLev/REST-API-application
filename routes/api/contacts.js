942ac921994b83afcf2c06831f965a6143988f86
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-02-17 13:31:08 +0200

    add hw02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,36 +1,112 @@
 const express = require("express");
-const {
-  getContact,
-  createContact,
-  deleteContact,
-  updateSomeContact,
-  updateStatusContact,
-  getAllContacts,
-} = require("../../controllers/contactsControllers");
-const { tryCatchWrapper } = require("../../helpers/index");
-const { validateBody } = require("../../middlewares/index");
-
-const { contactsSchema, updateStatusSchema } = require("../../schema/contacts");
-
+const { NotFound } = require("http-errors");
+const Joi = require("joi");
+const contactOperation = require("../../models/contacts");
 const router = express.Router();
 
-router.get("/", tryCatchWrapper(getAllContacts));
+const contactsSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.number().required(),
+});
 
-router.get("/:contactId", tryCatchWrapper(getContact));
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await contactOperation.listContacts();
+    res.json({
+      status: "succes",
+      code: 200,
+      data: {
+        result: contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post("/", validateBody(contactsSchema), tryCatchWrapper(createContact));
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contacts = await contactOperation.getContactById(contactId);
+    if (!contacts) {
+      throw new NotFound(
+        `Sorry, contact with contactId ${contactId} not found`
+      );
+    }
+    res.json({
+      status: "succes",
+      code: 200,
+      data: {
+        contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete("/:contactId", tryCatchWrapper(deleteContact));
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = contactsSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+    const added = await contactOperation.addContact(req.body);
+    res.status(201).json({
+      status: "succes",
+      code: 201,
+      data: { added },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put(
-  "/:contactId",
-  validateBody(contactsSchema),
-  tryCatchWrapper(updateSomeContact)
-);
-router.patch(
-  "/:contactId/favorite",
-  validateBody(updateStatusSchema),
-  tryCatchWrapper(updateStatusContact)
-);
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = contactsSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+    const { contactId } = req.params;
+
+    const update = await contactOperation.updateById(contactId, req.body);
+    if (!update) {
+      throw new NotFound(
+        `Sorry, contact with contactId ${contactId} not found`
+      );
+    }
+    res.json({
+      status: "succes",
+      code: 200,
+      data: { update },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const delet = await contactOperation.removeContact(contactId);
+    if (!delet) {
+      throw new NotFound(
+        `Sorry, contact with contactId ${contactId} not found`
+      );
+    }
+    res.json({
+      status: "succes",
+      code: 200,
+      message: "contact deleted",
+      data: { delet },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
